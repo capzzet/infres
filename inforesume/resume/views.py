f056b741eb255f92daf6174709a8d6e8ceac7ecf
Timur <capzzet@mail.ru>
2022-04-20 23:06:33 +0600

    update templates

--- inforesume/resume/views.py
+++ inforesume/resume/views.py
@@ -1,6 +1,11 @@
 from django.http import HttpResponse
-from django.shortcuts import render
-
+from django.shortcuts import render, redirect
+from docxtpl import DocxTemplate
+import os
+import mimetypes
+import pythoncom
+from docx2pdf import convert
+from pathlib import Path
 # Create your views here.
 menu = [{'title': "Главная", 'url_name': 'home'},
         {'title': "Резюме", 'url_name': 'resume'},
@@ -11,3 +16,6 @@ def index(request):
         'menu':menu
     }
     return render(request, 'resume/html/index.html')
+
+def resume(request):
+    return render(request, 'resume/html/resume.html')
