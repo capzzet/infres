00a180c8f72c3843a1cf6ae26151495d545e7206
Timur <capzzet@mail.ru>
2022-04-20 22:56:00 +0600

    added static and templates

--- inforesume/resume/views.py
+++ inforesume/resume/views.py
@@ -2,3 +2,12 @@ from django.http import HttpResponse
 from django.shortcuts import render
 
 # Create your views here.
+menu = [{'title': "Главная", 'url_name': 'home'},
+        {'title': "Резюме", 'url_name': 'resume'},
+        {'title': "О нас", 'url_name': 'about'},
+        {'title': "Блог", 'url_name': 'blog'}, ]
+def index(request):
+    context = {
+        'menu':menu
+    }
+    return render(request, 'resume/html/index.html')
